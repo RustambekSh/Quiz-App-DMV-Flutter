900d0b435d89fae3e5c09c3481f6503b0b88f70e
Rustambek Shodiyev <156027859+RustambekSh@users.noreply.github.com>
2025-01-14 08:41:54 -0600

    Update quiz_questions.dart

--- lib/models/quiz_questions.dart
+++ lib/models/quiz_questions.dart
@@ -4,9 +4,11 @@ class QuizQuestion {
   final String text;
   final List<String> answers;
 
+  
   List<String> getShuffledAnswers() {
     final shuffledList = List.of(answers);
     shuffledList.shuffle();
     return shuffledList;
   }
+  
 }
